e28607018f059ec6b36a1bb5d167c7df42bcd959
hasifcodes-beep <hasifcodes@gmail.com>
2026-06-26 09:05:55 +0800

    Refactor stack to handle string data
    Updated stack implementation to use strings instead of integers and modified related methods accordingly.

--- Task5_26_06_2026/Task5_linkedlist.cpp
+++ Task5_26_06_2026/Task5_linkedlist.cpp
@@ -2,46 +2,32 @@
 using namespace std;
 
 // Node structure for the linked list
-struct Node {
-    int data;
+class Node {
+	public:
+    string data;
     Node* next;
 };
 
 class Stack {
 private:
-    Node* top;      // Pointer to the top node
+    Node* top = nullptr;      // Pointer to the top node
+    // each stack initialize with an empty node
 
 public:
     // Constructor – initialises empty stack
-    Stack() {
-        top = nullptr;
-    }
-
-
 
     // Check if stack is empty
-    bool isEmpty() {
+    bool isEmpty() { 
         return top == nullptr;
     }
 
-    // For a linked list, stack is never "full" unless memory is exhausted.
-    // We'll check if we can allocate a new node.
-    bool isFull() {
-        // Try to allocate a temporary node; if it fails, the stack is full.
-        Node* temp = new(nothrow) Node;
-        if (temp == nullptr) {
-            return true;   // memory allocation failed
-        }
-        delete temp;
-        return false;
-    }
+    // For a linked list, stack is never full unless memory is exhausted, because it's dynamically-sized, unlike array
+    // Despite that, this is more hardware-related and hard to happen for modern computers
+
 
     // Push a value onto the stack
-    void push(int value) {
-        if (isFull()) {
-            cout << "Stack Overflow! Cannot push " << value << " (memory full)." << endl;
-            return;
-        }
+    void push(string value) {
+
         Node* newNode = new Node;
         newNode->data = value;
         newNode->next = top;
@@ -56,7 +42,7 @@ public:
             return;
         }
         Node* temp = top;
-        int poppedValue = temp->data;
+        string poppedValue = temp->data;
         top = top->next;
         delete temp;
         cout << poppedValue << " popped from stack." << endl;
@@ -78,9 +64,9 @@ public:
             return;
         }
         Node* current = top;
-        cout << "Stack elements (top to bottom): ";
+        cout << "Stack elements (top to bottom): \n";
         while (current != nullptr) {
-            cout << current->data << " ";
+            cout << current->data << endl;
             current = current->next;
         }
         cout << endl;
@@ -91,17 +77,20 @@ public:
 int main() {
     Stack s;
 
-    s.push(10);
-    s.push(20);
-    s.push(30);
+    s.push("Aimar");
+    s.push("Anjana");
+    s.push("Ahmad");
+    s.push("Jessy");
     s.display();
 
     s.peek();
 
     s.pop();
     s.pop();
+    s.pop();
     s.display();
-
+    
+	s.pop();
     s.pop();   // underflow
     s.peek();  // empty
 
